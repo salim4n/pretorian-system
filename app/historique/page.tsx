c42f69c059843ae230d55491aab375b99b2bb3b8
salim laimeche <slaimeche@webexpr.fr>
2024-06-21 19:40:44 +0200

    Fix indentation and remove eslint-disable

--- app/historique/page.tsx
+++ app/historique/page.tsx
@@ -20,8 +20,9 @@ import {
     ObjectDetection,
   } from '@tensorflow-models/coco-ssd'
 
-  interface HistoriqueProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface HistoriqueProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+/* eslint-disable */
 export default function Historique({ className }: HistoriqueProps) {
     const actualDate = new Date()
     const actualYear = actualDate.getFullYear()
