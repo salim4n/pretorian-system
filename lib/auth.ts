b8a9e03507916854a5740702b26e5c6b7a203611
salim laimeche <slaimeche@webexpr.fr>
2024-06-23 23:24:03 +0200

    Update authentication configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,12 +4,23 @@ import { TableStorageAdapter } from "@auth/azure-tables-adapter"
 import { AzureNamedKeyCredential, TableClient } from "@azure/data-tables"
 import Resend from "next-auth/providers/resend"
 //test update on azure env
+
+const azureAccount = process.env.AUTH_AZURE_ACCOUNT
+const azureAccessKey = process.env.AUTH_AZURE_ACCESS_KEY
+const azureTablesEndpoint = process.env.AUTH_AZURE_TABLES_ENDPOINT
+const resendKey = process.env.AUTH_RESEND_KEY
+
+if (!azureAccount) throw Error("Azure account not found")
+if (!azureAccessKey) throw Error("Azure access key not found")
+if (!azureTablesEndpoint) throw Error("Azure tables endpoint not found")
+if (!resendKey) throw Error("Resend key not found")
+
 const credential = new AzureNamedKeyCredential(
-    process.env.AUTH_AZURE_ACCOUNT,
-    process.env.AUTH_AZURE_ACCESS_KEY
+    azureAccount,
+    azureAccessKey
   )
   const authClient = new TableClient(
-    process.env.AUTH_AZURE_TABLES_ENDPOINT,
+    azureTablesEndpoint,
     "PretorianSystem",
     credential
   )
@@ -17,7 +28,7 @@ const credential = new AzureNamedKeyCredential(
 export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
         Resend({
-            apiKey: process.env.AUTH_RESEND_KEY,
+            apiKey: resendKey,
             from: "ignitionAI@resend.dev"
         }),
     ],
