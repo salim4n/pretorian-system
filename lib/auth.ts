c73cd4890aff0deea92e06ad8eed6b01ee3a3b13
salim laimeche <slaimeche@webexpr.fr>
2024-06-23 23:14:53 +0200

    Update authentication module with Azure environment test

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import NextAuth, { type NextAuthConfig } from "next-auth"
 import { TableStorageAdapter } from "@auth/azure-tables-adapter"
 import { AzureNamedKeyCredential, TableClient } from "@azure/data-tables"
 import Resend from "next-auth/providers/resend"
-
+//test update on azure env
 const credential = new AzureNamedKeyCredential(
     process.env.AUTH_AZURE_ACCOUNT,
     process.env.AUTH_AZURE_ACCESS_KEY
